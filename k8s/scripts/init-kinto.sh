b1b72583987b3890412048f436486918745b22dc
Thomas Glatt <tglatt@gmail.com>
2019-06-12 17:30:34 +0200

    ci: kinto init

--- k8s/scripts/init-kinto.sh
+++ k8s/scripts/init-kinto.sh
@@ -1,13 +1,13 @@
-# #!/bin/sh
+#!/bin/sh
 
-# INIT_KINTO_POD_STATUS=$(kubectl get job ds-aggregator-init-kinto"$1")
+INIT_KINTO_POD_STATUS=$(kubectl get job ds-aggregator-init-kinto"$1")
 
-# # Check if ds-aggregator-init-kinto job exists
-# if [ ! "$INIT_KINTO_POD_STATUS" ]
-# then
-#     kubectl apply -f k8s/kinto/job-init-kinto-ds-aggregator.yml
-# else
-#     kubectl delete job ds-aggregator-init-kinto"$1"
-#     kubectl apply -f k8s/kinto/job-init-kinto-ds-aggregator.yml
-# fi
+# Check if ds-aggregator-init-kinto job exists
+if [ ! "$INIT_KINTO_POD_STATUS" ]
+then
+    kubectl apply -f k8s/kinto/job-init-kinto-ds-aggregator.yml
+else
+    kubectl delete job ds-aggregator-init-kinto"$1"
+    kubectl apply -f k8s/kinto/job-init-kinto-ds-aggregator.yml
+fi
 
