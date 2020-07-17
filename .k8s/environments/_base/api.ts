36714ce45d7f46d4c057b0b9b3c2e05a6d647a4f
Douglas Duteil <douglasduteil@gmail.com>
2020-07-17 03:42:38 +0200

    ci(gitlab): run register on api Dockerfile (3)

---
+++ .k8s/environments/_base/api.ts
@@ -0,0 +1,29 @@
+import { AppComponentEnvironment } from "@socialgouv/kosko-charts/components/app/params";
+import { ok } from "assert";
+
+ok(process.env.CI_REGISTRY_IMAGE);
+ok(process.env.CI_COMMIT_SHA);
+
+const env: AppComponentEnvironment = {
+  containerPort: 4000,
+
+  image: {
+    name: `${process.env.CI_REGISTRY_IMAGE}/api`,
+    tag: process.env.CI_COMMIT_TAG
+      ? process.env.CI_COMMIT_TAG.slice(1)
+      : process.env.CI_COMMIT_SHA,
+  },
+
+  ingress: {
+    secretName: process.env.PRODUCTION ? "api-crt" : "wildcard-crt",
+  },
+
+  labels: {
+    component: "koa",
+  },
+  name: "api",
+
+  servicePort: 80,
+};
+
+export default env;
