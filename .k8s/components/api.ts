4dc192fa7d0ddbee51d805c7c20e34c08c387e7a
Douglas Duteil <douglasduteil@gmail.com>
2020-07-17 04:19:32 +0200

    build(k8s): make deployment use api-env s

--- .k8s/components/api.ts
+++ .k8s/components/api.ts
@@ -1,5 +1,21 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { ok } from "assert";
 
 const { deployment, ingress, service } = create(env.component("api"));
+
+ok(deployment.spec);
+ok(deployment.spec.template.spec);
+deployment.spec.template.spec.containers[0].envFrom = [
+  {
+    secretRef: {
+      name: "api-env",
+    },
+  },
+  {
+    configMapRef: {
+      name: "api-env",
+    },
+  },
+];
 export default [deployment, ingress, service];
