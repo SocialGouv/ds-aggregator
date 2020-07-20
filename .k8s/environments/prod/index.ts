72db1deaf2ca67f920ca9617d997398d52ff8b66
Douglas Duteil <douglasduteil@gmail.com>
2020-07-20 12:45:16 +0200

    fix(k8s): prod should not use dev globalenv...

--- .k8s/environments/prod/index.ts
+++ .k8s/environments/prod/index.ts
@@ -1,11 +1,8 @@
 import { GlobalEnvironment } from "@socialgouv/kosko-charts/types";
 import { ok } from "assert";
 
-import globalDevEnv from "../dev";
-
 ok(process.env.CI_PROJECT_NAME);
 
 export default {
-  ...globalDevEnv,
   subdomain: `wif.${process.env.CI_PROJECT_NAME}`,
 } as GlobalEnvironment;
